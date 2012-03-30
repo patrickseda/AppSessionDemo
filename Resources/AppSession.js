3823ca6ba12eb431a4b08782b4fddf665e06ad66
Patrick Seda <patrickseda@gmail.com>
2012-03-29 22:31:02 -0500

    Beautify format, no functional changes.

--- Resources/AppSession.js
+++ Resources/AppSession.js
@@ -1,52 +1,54 @@
 /**
-  * Provides a client-side session which is capable of timing out due to inactivity.
-  *
-  * Sample usage:
-  *     var AppSession = require('AppSession').AppSession;
-  *     var appSession = new AppSession();
-  *     appSession.setTimeoutMs(30000); // A value of 0 means never timeout.
-  *
-  *     // Start a new session, e.g. after a login.
-  *     appSession.startNewSession('user67');
-  *
-  *     // Touch the session whenever an app UI events occurs.
-  *     appSession.touch();
-  *
-  *     // Inspect session values.
-  *     var userId = appSession.getUserId();  // Will be 'user67'.
-  *     var isLoggedIn = appSession.isLive(); // Will be 'true'.
-  *
-  *     // Terminate the session, e.g. after a logout.
-  *     appSession.endSession();
-  *     isLoggedIn = appSession.isLive(); // Will be 'false'.
-  *
-  * @author Patrick Seda
-  */
+ * Provides a client-side Session which is capable of timing out due to inactivity.
+ *
+ * Sample usage:
+ *     // Load the module and create an instance.
+ *     var AppSession = require('AppSession').AppSession;
+ *     var appSession = new AppSession();
+ *     appSession.setTimeoutMs(30000); // A value of 0 means never timeout.
+ *
+ *     // Start a new Session, e.g. after a login.
+ *     // You can track the logged-in user if you wish.
+ *     appSession.startNewSession('user67');
+ *
+ *     // Touch the Session whenever an app UI events occurs.
+ *     appSession.touch();
+ *
+ *     // Inspect Session values.
+ *     appSession.getUserId();  // Will be 'user67'.
+ *     appSession.isLive();     // Will be 'true' as long as there is 'touch' activity.
+ *
+ *     // Terminate the Session, e.g. after a logout.
+ *     appSession.endSession();
+ *     appSession.isLive();  // Will be 'false'.
+ *
+ * @author Patrick Seda
+ */
 exports.AppSession = function() {
-    // +-----------------------+
-    // | Private members.      |
-    // +-----------------------+
-    // The TTL for a session. A value of 0 means never timeout.
-    var sessionTimeoutMs = 600000; // 600000 ms = 10 min
-    
-    var lastAccessTime = null;
-    var userIdKey = 'SESSION_USER_ID';
-    var heartbeatTimer = null;
-    
-	// Determine if the session can ever timeout.
+	// +-----------------------+
+	// | Private members.      |
+	// +-----------------------+
+	// The TTL for a Session. A value of 0 means never timeout.
+	var sessionTimeoutMs = 600000; // 600000 ms = 10 min
+
+	var lastAccessTime = null;
+	var userIdKey = 'SESSION_USER_ID';
+	var heartbeatTimer = null;
+
+	// Determine if the Session can ever timeout.
 	var usingTimeout = function() {
 		return sessionTimeoutMs > 0;
 	};
-    
+	
 	// Remove the entry for the UserID.
 	var clearUserId = function() {
 		Ti.App.Properties.removeProperty(userIdKey);
 	};
 	
-	// Start the internal heartbeat thread if timeout is enabled.
+	// Start the internal heartbeat thread (if timeout is enabled).
 	var startHeartbeat = function() {
 		if (usingTimeout()) {
-			var mostFrequentPeriodMs = 15000; // 15000 ms = 15 sec
+			var mostFrequentPeriodMs = 15000;
 			var heartbeatMs = Math.max(mostFrequentPeriodMs, sessionTimeoutMs/4);
 			heartbeatTimer = setInterval(function() {
 				if (!isSessionLive()) {
@@ -66,19 +68,19 @@ exports.AppSession = function() {
 	};
 	
 	
-    // +-----------------------+
-    // | Public members.       |
-    // +-----------------------+
-    // Touch the session to indicate it is still active.
-    var touchSession = function() {
+	// +-----------------------+
+	// | Public members.       |
+	// +-----------------------+
+	// Touch the Session to indicate it is still active.
+	var touchSession = function() {
 		if (isSessionLive()) {
-    		lastAccessTime = new Date(); // now
-    	} else {
-    		Ti.API.info('TOUCH (dead session)');
-    	}
-    };
-
-	// Get the UserID.
+			lastAccessTime = new Date(); // now
+		} else {
+			Ti.API.info('TOUCH (dead Session)');
+		}
+	};
+	
+	// Get the UserID for the Session.
 	var getUserId = function() {
 		if (isSessionLive()) {
 			touchSession();
@@ -88,19 +90,19 @@ exports.AppSession = function() {
 		return Ti.App.Properties.getString(userIdKey, null);
 	};
 	
-	// Set the session timeout.
+	// Set the Session timeout.
 	function setTimeoutMs(timeoutMs) {
 		if (timeoutMs && (timeoutMs >= 0)) {
 			sessionTimeoutMs = timeoutMs;
 		}
 	};
 	
-	// Get the session timeout.
+	// Get the Session timeout.
 	var getTimeoutMs = function() {
 		return sessionTimeoutMs;
 	};
 	
-	// Start a new session.
+	// Start a new Session.
 	var startNewSession = function(userIdOfSession) {
 		if (isSessionLive()) {
 			endSession();
@@ -110,20 +112,20 @@ exports.AppSession = function() {
 		lastAccessTime = new Date(); // now
 		startHeartbeat();
 	};
-    
-    // Determine if the session has expired since the last request.
-    var isSessionLive = function() {
-    	if (lastAccessTime === null) {
-    		return false;
-    	}
-    	if (!usingTimeout()) {
-    		return true;
-    	}
+	
+	// Determine if the Session has expired since the last request.
+	var isSessionLive = function() {
+		if (lastAccessTime === null) {
+			return false;
+		}
+		if (!usingTimeout()) {
+			return true;
+		}
 		var now = new Date();
 		return (now.getTime() - lastAccessTime.getTime() < sessionTimeoutMs);
-    };
-    
-	// Remove all information related to the current session.
+	};
+	
+	// Remove all information related to the current Session.
 	var endSession = function() {
 		stopHeartbeat();
 		lastAccessTime = null;
@@ -131,19 +133,19 @@ exports.AppSession = function() {
 		Ti.API.info('Your Session has ended!');
 	};
 	
-   
-    // +-----------------------+
-    // | Public members.       |
-    // +-----------------------+
-    return {
-    	getUserId : getUserId,
-    	
-    	setTimeoutMs : setTimeoutMs,
-    	getTimeoutMs : getTimeoutMs,
-    	
+	
+	// +-----------------------+
+	// | Public API.           |
+	// +-----------------------+
+	return {
+		getUserId : getUserId,
+
+		setTimeoutMs : setTimeoutMs,
+		getTimeoutMs : getTimeoutMs,
+
 		startNewSession : startNewSession,
 		touch : touchSession,
 		isLive : isSessionLive,
 		endSession : endSession
-    };
+	};
 };
